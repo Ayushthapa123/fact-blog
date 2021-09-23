3902a962db49245c7fddc4b427989f0d08d2d8e6
Ayush Thapa <eplramailo@gmail.com>
2021-09-23 21:53:29 +0545

    businessideas folder added

---
+++ pages/businessideas/[slug].js
@@ -0,0 +1,105 @@
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS } from "@contentful/rich-text-types";
+import Head from 'next/head';
+import { createClient } from 'contentful';
+import Layout from '../../components/templates/Blogtemplate';
+import styles from '../../sass/slugs.module.scss';
+
+
+
+const client = createClient({
+  space:process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken:process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+
+});
+
+export async function getStaticPaths() {
+  let data = await client.getEntries({
+    content_type: "businessideas",
+  });
+
+  return {
+    paths: data.items.map((item) => ({
+      params: { slug: item.fields.slug },
+    })),
+    fallback:false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  let data = await client.getEntries({
+    content_type: "businessideas",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: {
+      article: data.items[0],
+    },
+    revalidate:50, 
+    // Incremental page re-generation after deployment
+  };
+}
+
+export default function Article({ article }) {
+  if (!article) return <div>404</div>;
+
+
+
+  return (
+    <div className='blog-post'>
+
+<Head>
+  <title>{article.fields.title}</title>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9683433840347502"
+     crossorigin="anonymous"></script>
+</Head>
+
+<Layout>
+
+<div className={styles.header}>
+<h1>{article.fields.title}</h1>
+<p>Published date: {article.fields.date} </p>
+</div>
+
+   <section className={styles.content}>
+      <div className={styles.article}>
+        {documentToReactComponents(article.fields.body,
+        
+        {
+          renderNode: {
+            [BLOCKS.EMBEDDED_ASSET]: (node) => (
+              <img
+              alt='image'
+                src={"https:" + node.data.target.fields.file.url}
+                width={node.data.target.fields.file.details.image.width}
+                height={node.data.target.fields.file.details.image.height}
+              />
+            ),
+          },
+        }
+        
+        )}
+      </div>
+
+<div className={styles.sidebar}>
+{/* <h2>Grow Web Technology</h2> */}
+</div>
+
+ </section>
+
+ </Layout>
+
+
+      <style jsx>{`
+        .blog-post {
+         width:95%;
+         margin:auto;
+        }
+      `}</style>
+
+
+
+    </div>
+  );
+}
---
+++ pages/businessideas/index.js
@@ -0,0 +1,77 @@
+import Head from "next/head";
+import Link from "next/link";
+import Image from 'next/image'
+import { createClient } from 'contentful';
+
+import styles from '../../sass/blogsindex.module.scss'
+
+import FactTemplate from '../../components/templates/factTemplate'
+
+
+
+const client = createClient({
+  space:process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken:process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+
+});
+
+export async function getStaticProps() {
+  let data = await client.getEntries({
+    content_type: "businessideas",
+  });
+
+  return {
+    props: {
+      articles: data.items,
+    },
+    revalidate: 50,
+  };
+}
+
+export default function Home({ articles }) {
+
+// console.log(articles[0])
+// console.log(articles[0].fields.coverphoto.fields.file.details.image.height)
+
+  return (
+    <div className='blog-list'>
+      <Head>
+        <title>Business Ideas</title>
+    
+      </Head>
+
+<FactTemplate>
+
+      <main className={styles.blogs}>
+   
+          {articles.map((article) => (
+            <div key={article.sys.id}>
+         
+
+              <Link href={"/businessideas/" + article.fields.slug}>
+
+                <a>
+                
+                <h2>|{article.fields.title}</h2>
+
+                <div className={styles.imgcontainer}>
+                <Image src={'https:' + article.fields.coverphoto.fields.file.url} alt='Fact Image' className={styles.image}
+                 layout='fill'
+                />
+                 </div>
+
+                </a>
+              </Link> 
+            </div> 
+          ))}
+    
+      </main>
+      </FactTemplate>
+
+
+
+   
+
+    </div>
+  );
+}
